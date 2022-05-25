a0cdb5b4c329ac2bab4f6086245842821079f405
Clesio FG <cfgnet@sdcnet.com.br>
2022-05-24 21:23:07 -0300

    retorno do cors com url de publicacao

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express'
-//import cors from 'cors'
+import cors from 'cors'
 import { routes } from './routes';
 
 const app = express();
 
-//app.use(cors());
+app.use(cors({ origin: 'https://nlw-return-inpulse-lsuq4rmjv-cfgnet.vercel.app'}));
 app.use(express.json());
 app.use(routes);
 
